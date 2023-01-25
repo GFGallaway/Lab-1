74a20a4ed98e540a63ee893f9b564715acbca9af
Gabby F G <123026505+GFGallaway@users.noreply.github.com>
2023-01-25 15:53:53 -0500

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,34 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#the author's name is : Gabby
+print("Hello, World")
+Hello, World
+print ("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today", "is", "Wednesday")
+Today is Wednesday
+print("today"+"is"+"Wednesday")
+todayisWednesday
+print("a\nb\nc")
+a
+b
+c
+print("Hello"+"world")
+Helloworld
+print("Hello\n"+"\nWorld!")
+Hello
+
+World!
+print("123\n45\n6")
+123
+45
+6
+print("50 pounds is", 50*0.453592, "kilograms")
+50 pounds is 22.6796 kilograms
